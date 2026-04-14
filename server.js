beebc8808ec73fd28334da8565e69d569fc5a710
Wael Muhsen <waelmuhsen.576@gmail.com>
2026-04-14 08:07:56 +0200

    dsw

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-/* const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL)
 const db= mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
-*/
+
 
 app.use('/', indexRouter);
 const PORT = process.env.PORT || 3000;
